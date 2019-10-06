14354b64cba2a38b6e8c045cf4ee7447f5d09d71
Hunter Chang <hunter@madwiremedia.com>
2019-10-06 07:59:09 -0600

    add .default to gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,6 @@
 const React = require("react")
 const GlobalContextProvider = require("./src/context/GlobalContextProvider")
+  .default
 
 exports.wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>{element}</GlobalContextProvider>
